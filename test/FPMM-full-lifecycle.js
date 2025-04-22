9524f3031ee67f2d61f5136ebfe2f58359e4afb4
Tadej Vengust <vengust.tadej@gmail.com>
2025-04-22 11:29:46 +0200

    update full lifecycle test

--- test/FPMM-full-lifecycle.js
+++ test/FPMM-full-lifecycle.js
@@ -73,8 +73,14 @@ describe("FPMM Full Lifecycle", function () {
     await collateralToken.connect(user1).deposit({ value: buyAmount });
     await collateralToken.connect(user1).approve(fpmm.address, buyAmount);
 
+    // Get initial fee pool weight
+    const initialFeePoolWeight = await fpmm.getFeePoolWeight();
+
     const maxBuy = await fpmm.calcBuyAmount(buyAmount, sellOutcomeIndex);
     await fpmm.connect(user1).buy(buyAmount, sellOutcomeIndex, maxBuy);
+
+    // Calculate buy fee in wei
+    const buyFee = buyAmount.mul(feeFactor).div(ethers.utils.parseEther("1"));
 
     const traderTokenBalance = await conditionalTokens.balanceOf(user1.address, positionIds[sellOutcomeIndex]);
     const returnAmount = ethers.utils.parseUnits("5", 6);
@@ -84,10 +90,65 @@ describe("FPMM Full Lifecycle", function () {
     expect(traderTokenBalance).to.be.gte(tokensToSell);
     await fpmm.connect(user1).sell(returnAmount, sellOutcomeIndex, tokensToSell);
 
+    // Calculate sell fee in wei
+    const sellFee = returnAmount.mul(feeFactor).div(ethers.utils.parseEther("1").sub(feeFactor));
+
+    // Get fee pool weight after trades
+    const afterTradesFeePoolWeight = await fpmm.getFeePoolWeight();
+    
+    // Verify fees were added to pool
+    expect(afterTradesFeePoolWeight.sub(initialFeePoolWeight)).to.be.closeTo(buyFee.add(sellFee), 1000);
+
     await conditionalTokens.connect(oracle).reportPayouts(questionId, [1, 0, 0]);
 
-    const shares = await fpmm.balanceOf(user2.address);
-    await fpmm.connect(user2).removeFunding(shares);
+    // Get user2's share of total supply
+    const user2Shares = await fpmm.balanceOf(user2.address);
+    const totalSupply = await fpmm.totalSupply();
+    
+    // Calculate expected fees for user2 based on their share of total supply
+    const feePoolIncrease = afterTradesFeePoolWeight.sub(initialFeePoolWeight);
+    const expectedUser2Fees = feePoolIncrease.mul(user2Shares).div(totalSupply);
+    const expectedTreasuryFees = expectedUser2Fees.mul(treasuryPercent).div(10000);
+    const expectedUser2NetFees = expectedUser2Fees.sub(expectedTreasuryFees);
+
+    // Get initial balances
+    const initialUser2Balance = await collateralToken.balanceOf(user2.address);
+    const initialTreasuryBalance = await collateralToken.balanceOf(treasury.address);
+
+    // Get initial withdrawn fees for user2
+    const initialWithdrawnFees = await fpmm.feesWithdrawableBy(user2.address);
+
+    // Remove funding and withdraw fees
+    await fpmm.connect(user2).removeFunding(user2Shares);
+
+    // Verify fee distribution
+    const finalUser2Balance = await collateralToken.balanceOf(user2.address);
+    const finalTreasuryBalance = await collateralToken.balanceOf(treasury.address);
+
+
+    // Check that user2 received their share of fees
+    const user2FeeReceived = finalUser2Balance.sub(initialUser2Balance);
+    expect(user2FeeReceived).to.be.closeTo(expectedUser2NetFees, 1000);
+
+    // Check that treasury received their share of fees
+    const treasuryFeeReceived = finalTreasuryBalance.sub(initialTreasuryBalance);
+    expect(treasuryFeeReceived).to.be.closeTo(expectedTreasuryFees, 1000);
+
+    // Verify fee pool weight is updated
+    const finalFeePoolWeight = await fpmm.getFeePoolWeight();
+    expect(finalFeePoolWeight).to.be.lt(afterTradesFeePoolWeight);
+
+    // Verify withdrawn fees are updated
+    const finalWithdrawnFees = await fpmm.feesWithdrawableBy(user2.address);
+    expect(finalWithdrawnFees).to.equal(0);
+
+    // Add a test to verify that withdrawing again doesn't give more fees
+    await fpmm.withdrawFees(user2.address);
+    const balanceAfterSecondWithdraw = await collateralToken.balanceOf(user2.address);
+    expect(balanceAfterSecondWithdraw).to.equal(finalUser2Balance);
+
+    // Add a test to verify that the total fees received match the initial withdrawable fees
+    expect(user2FeeReceived).to.equal(initialWithdrawnFees);
 
     await conditionalTokens.connect(user2).redeemPositions(
       collateralToken.address,
@@ -98,47 +159,5 @@ describe("FPMM Full Lifecycle", function () {
 
     const bal = await collateralToken.balanceOf(user2.address);
     expect(bal).to.be.gt(0);
-
-    // Simulate exact fee calculation logic
-    const feePoolWeight = await fpmm.getFeePoolWeight();
-    const totalShares = await fpmm.totalSupply();
-    const user1Shares = await fpmm.balanceOf(user1.address);
-    const user2Shares = await fpmm.balanceOf(user2.address);
-
-    const user1Fees = computeUserFees(feePoolWeight, user1Shares, totalShares, treasuryPercent);
-    const user2Fees = computeUserFees(feePoolWeight, user2Shares, totalShares, treasuryPercent);
-
-    const feeUser1 = await fpmm.feesWithdrawableBy(user1.address);
-    const feeUser2 = await fpmm.feesWithdrawableBy(user2.address);
-
-    const treasuryBalanceBefore = await collateralToken.balanceOf(treasury.address);
-    const user1BalanceBefore = await collateralToken.balanceOf(user1.address);
-    const user2BalanceBefore = await collateralToken.balanceOf(user2.address);
-
-    if (feeUser1.gt(0)) {
-      await fpmm.connect(user1).withdrawFees(user1.address);
-    }
-    if (feeUser2.gt(0)) {
-      await fpmm.connect(user2).withdrawFees(user2.address);
-    }
-
-    const user1BalanceAfter = await collateralToken.balanceOf(user1.address);
-    const user2BalanceAfter = await collateralToken.balanceOf(user2.address);
-    const treasuryBalanceAfter = await collateralToken.balanceOf(treasury.address);
-
-    expect(user1BalanceAfter.sub(user1BalanceBefore)).to.equal(user1Fees.userCut);
-    expect(user2BalanceAfter.sub(user2BalanceBefore)).to.equal(user2Fees.userCut);
-
-    const actualTreasuryFee = treasuryBalanceAfter.sub(treasuryBalanceBefore);
-    const expectedTreasuryFee = user1Fees.treasuryCut.add(user2Fees.treasuryCut);
-
-    expect(actualTreasuryFee).to.equal(expectedTreasuryFee);
   });
 });
-
-function computeUserFees(feePool, userShares, totalShares, treasuryPercent) {
-    const raw = feePool.mul(userShares).div(totalShares);
-    const treasuryCut = raw.mul(treasuryPercent).div(10000);
-    const userCut = raw.sub(treasuryCut);
-    return { raw, treasuryCut, userCut };
-  }
